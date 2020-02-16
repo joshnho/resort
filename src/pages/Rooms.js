8209839488e3b5df2be00c2d0df7e7343c32e062
Joshua Nho <joshnho@gmail.com>
2020-02-15 22:43:20 -0500

    added banner and banner photo

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 
-export default function Rooms() {
+const Rooms = () => {
     return (
-        <div>
-            hello from rooms page
-        </div>
+        <Hero hero="roomsHero">
+            <Banner title='Our Rooms'>
+                <Link to='/' className='btn-primary'>return home</Link>
+            </Banner>
+        </Hero>
     )
 }
+
+export default Rooms;
