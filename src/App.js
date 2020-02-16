e1b977c553929c0f3489f867921bd91cee896d78
Joshua Nho <joshnho@gmail.com>
2020-02-15 22:02:35 -0500

    added pages, components, updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import Navbar from './components/Navbar';
+import {Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
-    <>Hello</>
+    <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Route component={Error} />
+    </Switch>
+    </>
   );
 }
 
