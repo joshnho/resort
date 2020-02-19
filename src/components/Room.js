7a909586dd4d581bd2235233855232ddaf58ac98
Joshua Nho <joshnho@gmail.com>
2020-02-18 22:36:09 -0500

    getroom function on context.js

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 export default function Room({room}) {
     const {name, slug, images, price} = room
@@ -17,4 +18,13 @@ export default function Room({room}) {
             <p className="room-info">{name}</p>
         </article>
     )
+}
+
+Room.propTyes = {
+    room: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        slug: PropTypes.string.isRequired,
+        images: PropTypes.arrayOf(PropTypes.string).isRequired,
+        price: PropTypes.number.isRequired
+    })
 }
