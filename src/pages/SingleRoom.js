01aa31e4d9602c913c5a555eba23aadadc80a9e3
Joshua Nho <joshnho@gmail.com>
2020-02-19 00:03:11 -0500

    updated single room component and styled hero component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from '../context'
+import StyledHero from '../components/StyledHero'
 
 export default class SingleRoom extends Component {
+    state = {
+        slug: this.props.match.params.slug,
+        defaultBcg
+    }
+
+    static contextType = RoomContext
+
     render() {
+        const {getRoom} = this.context
+        const room = getRoom(this.state.slug)
+
+        if (!room){
+            return <div className="error">
+                        <h3>Room not found</h3>
+                        <Link to="/rooms" className="btn-primary">Back to Rooms</Link>
+                    </div>
+        }
+
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
+        
         return (
-            <div>
-                Hello from single room page
-            </div>
+            <>
+                <StyledHero img={images[0] || this.state.defaultBcg}>
+                    <Banner title={`${name} Room`}>
+                        <Link to="/rooms" className="btn-primary">Back to Rooms</Link>
+                    </Banner>
+                </StyledHero>
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {images.map((img, i) => {
+                            return <img key={`img-${i}`} src={img} alt={name} />
+                        })}
+                    </div>
+                </section>
+            </>
         )
     }
 }
